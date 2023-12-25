175339023fa5951ac83a86d72580d90c49503dcf
mahasanea <154835925+mahasanea@users.noreply.github.com>
2023-12-25 22:36:31 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h3>form bilgileri: </h3>";
+echo "<b>Name:</b>".$_POST["ad"];
+echo "<br>";
+echo "<b>Last Name:</b>".$_POST["LastName"] ;
+echo "<br>";
+echo "<b>Eposta:</b>".$_POST["email"] ;
+echo "<br>";
+echo "<b>Phone Number:</b>".$_POST["phoneNum"] ;
+echo "<br>";
+
+echo "<b>cender:</b>".$_POST["Cender"] ;
+echo "<br>";
+echo "<b>massege:</b>".$_POST["massege"] ;
+
+?>
